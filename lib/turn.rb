2bebe5b487b664e08b1191349ab9b40a144817af
Fei Qi <feiqi9047@gmail.com>
2018-11-27 20:14:33 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -16,7 +16,7 @@ def position_taken?(board, index)
 end
 
 def valid_move?(board, index)
-  if index.between(0,8) && position_taken?(board, index)
+  if index.between(0,8) && !position_taken?(board, index)
     TRUE
   else
     FALSE
