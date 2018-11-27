b3432913f1c71279b5c1a579fc7dc9cc4781869d
Fei Qi <feiqi9047@gmail.com>
2018-11-27 20:30:45 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -32,7 +32,7 @@ def move(board, index, token="X")
 end
 
 def turn(board)
-  puts "Please enter 1-9"
+  puts "Please enter 1-9:"
   input = gets.strip
   if valid_move?(board, index)
     move(board, index, token)
